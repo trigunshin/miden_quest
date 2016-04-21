61269889d09f739c25d4529b19c15fbd82b50896
pcrane <pcrane@goscoutgo.com>
2016-04-21 00:46:41 -0400

    added 3 other ts to track script, as well as instructions.

--- track_resources.js
+++ track_resources.js
@@ -1,3 +1,15 @@
+/*
+INSTRUCTIONS
+Copy/paste this whole  section into console & hit enter. It should track gather rates for Gathering, Fishing,
+Mining and Woodcutting. It will print the results after every gather; please note that this will never give
+exact values, and you should probably wait for 300-500+ attempts on a tile to draw conclusions.
+
+To reset the values (necessary if you move tiles) you can copy/paste the whole script again or just enter
+this into the console:  "clearTSResults();".
+
+THIS IS PROBABLY (100%) NOT COMPATIBLE WITH RYALANE'S CHAT SCRIPT
+//*/
+
 // set up handler array & hook game's handler in
 var eventHandlers = [ServerReceptionHandler];
 function onmsg(evt) {
@@ -7,6 +19,7 @@ function onmsg(evt) {
 };
 ws.onmessage=onmsg;
 
+// results aren't stored under the resource because we aren't tracking tile changes/types
 var tsResults = {
 	actions:0,
 	1: 0,
@@ -16,6 +29,15 @@ var tsResults = {
 	5: 0,
 	lumber:{
 		items: ['Pine', 'Oak', 'Maple', 'Ironwood', 'Ygddrasil']
+	},
+	ore: {
+		items: ['Iron', 'Silver', 'Obsidian', 'Mythril', 'Ethernium']
+	},
+	plant: {
+		items: ['Plant Stem', 'Cotton', 'Living Leather', 'Silver Vine', 'Nimbus Fruit']
+	},
+	fish: {
+		items: ['Tuna', 'Salmon', 'Flyfish', 'Marlin', 'Dragonfish']
 	}
 };
 function clearTSResults() {
@@ -24,7 +46,6 @@ function clearTSResults() {
 		tsResults[i] = 0;
 	}
 }
-
 function parseTSLog(datum) {
 	var arr = datum.split('|');
 	if (arr[0] != 'NLOG') {return;}
@@ -36,13 +57,24 @@ function parseTSLog(datum) {
 	if(channel == 3) return;
 	else if(channel == 2) {
 		tsResults.actions += 1;
+		// which array should be used?
+		var itemTypes = null;
 		if(msg.indexOf('You cut') >= 0) {
-			for(var i=0,iLen=tsResults.lumber.items.length;i<iLen;i++) {
-				if(msg.indexOf(tsResults.lumber.items[i]) >= 0) {
-					tsResults[i+1] += 1;
-				}
+			itemTypes = tsResults.lumber.items;
+		} else if(msg.indexOf('You mined') >= 0) {
+			itemTypes = tsResults.ore.items;
+		} else if(msg.indexOf('You gathered') >= 0) {
+			itemTypes = tsResults.plant.items;
+		} else if(msg.indexOf('You caught') >= 0) {
+			itemTypes = tsResults.fish.items;
+		}
+
+		for(var i=0,iLen=itemTypes.length;i<iLen;i++) {
+			if(msg.indexOf(itemTypes[i]) >= 0) {
+				tsResults[i+1] += 1;
 			}
 		}
+
 		console.info(
 			't1:', (100*tsResults[1]/tsResults.actions).toFixed(2),
 			't2:', (100*tsResults[2]/tsResults.actions).toFixed(2),
