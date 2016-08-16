01fb9be21880e0054e882a55d7b0acb9deb53ea1
pcrane <pcrane@goscoutgo.com>
2016-08-16 14:46:22 -0400

    added title+name to printed output of profile parser.

--- parseProfile.user.js
+++ parseProfile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name MQO Profile Parser
 // @namespace https://github.com/trigunshin/miden_quest
-// @description MQO profile parser. Currently prints TS attempts to console
+// @description MQO profile parser. Currently prints TS attempts and title+username to console
 // @homepage https://trigunshin.github.com/miden_quest
 // @version 1
 // @downloadURL http://trigunshin.github.io/miden_quest/parseProfile.user.js
@@ -11,11 +11,9 @@
 // @include https://www.midenquest.com/Game.aspx
 // @grant GM_log
 // ==/UserScript==
-function parseProfile(data) {
-	var arr = data.split('|');
-	if (arr[0] != 'LOADPAGE') return;
+function getTSAttempts(data) {
 	var tmp = $(data).find("div:contains('tradeskill attempts')");
-	if(tmp.length === 0) return;
+	if(tmp.length === 0) return -1;
 
 	var tsDiv = tmp;
 	var tsNode = tsDiv[tsDiv.length-1];
@@ -23,7 +21,28 @@ function parseProfile(data) {
 	var tsSiblings = tsNodeParent.childNodes;
 	var tsAttemptNode = tsSiblings[3];
 	var tsAttempts = tsAttemptNode.innerText.replace(/ /g, '');
-	console.info(tsAttempts, 'attempts');
+	return tsAttempts;
+}
+function getName(data) {
+	var barDiv = $("div.CrumbBar")[0];
+	var parent = barDiv.parentElement;
+	var barParentChildren = parent.childNodes;
+	// from top level down to target child
+	// order is div,script,div plus some misc text in between
+	var profileContentChild = barParentChildren[4];
+	// now we want first children of next 3 nodes to hit the title div
+	var titleDiv = profileContentChild.childNodes[1].childNodes[1].childNodes[1]
+	return titleDiv.innerText;
+}
+function parseProfile(data) {
+	var arr = data.split('|');
+	if (arr[0] != 'LOADPAGE') return;
+	var msg = arr[1];
+
+	var tsAttempts = getTSAttempts(msg);
+	var name = getName(msg);
+
+	console.info(tsAttempts, 'attempts by', name);
 }
 
 function listen_profile_msg(evt) {
