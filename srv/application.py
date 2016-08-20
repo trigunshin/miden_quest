0f0168805dd80043fd35831b5cfdd77e2d58b511
pcrane <pcrane@goscoutgo.com>
2016-08-20 01:46:12 -0400

    initial commit of CORS EBS server.

---
+++ srv/application.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+resource_data = {
+	'orb': 0,
+	'scroll': 0,
+	'me': 0,
+	'relic': 0,
+	'gem': 0,
+	'wood': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0},
+	'fish': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0},
+	'plant': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0},
+	'ore': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
+}
+simple_keys = ['orb', 'scroll', 'me', 'relic', 'gem']
+deep_keys = ['wood', 'fish', 'plant', 'ore']
+tiers = [str(i) for i in range(1,6)]
+
+def _update_prices(data):
+	for k in simple_keys:
+		resource_data[k] = int(data.get(k, resource_data[k]))
+	for k in deep_keys:
+		for i in tiers:
+			old_value = resource_data.get(k, {}).get(i, 0)
+			new_value = data.get(k, {}).get(i, old_value)
+
+			resource_data[k][i] = int(new_value)
+
+	return resource_data
+
+@app.route('/update_market_prices', methods=['POST'])
+def update_market_prices():
+	data = request.json
+	data = _update_prices(data)
+	return jsonify(data)
+
+@app.route('/update_market_prices', methods=['GET'])
+def get_market_prices():
+	return jsonify(resource_data)
+
+@app.route('/')
+def index():
+    return jsonify(resource_data)
+
+application = app
+if __name__ == '__main__':
+    app.run()
