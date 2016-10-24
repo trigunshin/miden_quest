b4cc2525aeb766272740dd0c3f06967cce76e7e4
pcrane <pcrane@goscoutgo.com>
2016-10-24 16:18:35 -0400

    removed outdated config.py creds, added it to gitignore

--- srv/config.py
+++ srv/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://mqo_admin:mqo_admin_p4ssw0rd@mqo-data.cmfjczslvipm.us-east-1.rds.amazonaws.com:5432/miden"
+SQLALCHEMY_DATABASE_URI = ""
 SQLALCHEMY_TRACK_MODIFICATIONS = False
