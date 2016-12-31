1477b7343a27d4b2c5d180e87bfdd85341ca3688
pcrane <pcrane@goscoutgo.com>
2016-12-31 13:26:09 -0500

    further url fix for mqo market data

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -147,7 +147,7 @@ const Container = React.createClass({
         </Provider>;
     }
 });
-var aws_endpoint = 'https://www.midenquest.info/market';
+var aws_endpoint = 'https://www.midenquest.info/market/';
 
 $.ajax({
     url: aws_endpoint,
