1345130c9594d7e3f1ac7cb72bf4e8fbd527ae46
pcrane <pcrane@goscoutgo.com>
2017-02-17 12:37:46 -0500

    fixed homepage url

--- userScripts/expeditionLoader.user.js
+++ userScripts/expeditionLoader.user.js
@@ -2,8 +2,8 @@
 // @name MQO Expedition Helper
 // @namespace https://github.com/trigunshin/miden_quest
 // @description MQO expedition level helper; saves clicks setting up expedition
-// @homepage https://trigunshin.github.com/miden_quest_auto
-// @version 2
+// @homepage https://trigunshin.github.com/miden_quest
+// @version 3
 // @downloadURL http://trigunshin.github.io/miden_quest/userScripts/expeditionLoader.user.js
 // @updateURL http://trigunshin.github.io/miden_quest/userScripts/expeditionLoader.user.js
 // @include http://midenquest.com/Game.aspx
@@ -12,7 +12,7 @@
 // ==/UserScript==
 (function () {
     // include version in ajax request to 'bust' out of old, cached versions
-    const version = 2;
+    const version = 3;
     const url = "https://trigunshin.github.io/miden_quest/build/expeditions.js?&preventCache="+version;
     var scriptElement = document.createElement("script");
     scriptElement.type = "text/javascript";
