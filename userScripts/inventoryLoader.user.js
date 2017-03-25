67611981f2791b276dfc577574aeba0b73c67381
pcrane <pcrane@goscoutgo.com>
2017-03-25 07:56:14 -0400

    update the loader

--- userScripts/inventoryLoader.user.js
+++ userScripts/inventoryLoader.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/trigunshin/miden_quest
 // @description MQO inventory helper; reforges, enchants, quicksearch (load any item first!)
 // @homepage https://trigunshin.github.com/miden_quest
-// @version 19
+// @version 20
 // @downloadURL http://trigunshin.github.io/miden_quest/userScripts/inventoryLoader.user.js
 // @updateURL http://trigunshin.github.io/miden_quest/userScripts/inventoryLoader.user.js
 // @include http://midenquest.com/Game.aspx
@@ -12,7 +12,7 @@
 // ==/UserScript==
 (function () {
     // include version in ajax request to 'bust' out of old, cached versions
-    const version = 19;
+    const version = 20;
     const url = "https://trigunshin.github.io/miden_quest/build/inventory.js?&preventCache="+version;
     var scriptElement = document.createElement("script");
     scriptElement.type = "text/javascript";
