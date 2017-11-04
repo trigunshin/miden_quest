919e329a7172953f5afb4fa3a5efb76fe91273f0
pcrane <trigunshin@gmail.com>
2017-11-04 14:09:02 -0400

    added insecure TICK box for prettier market graphs

--- userScripts/parseMarketOverview.user.js
+++ userScripts/parseMarketOverview.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/trigunshin/miden_quest
 // @description MQO market overview parser. Sends data to server.
 // @homepage https://trigunshin.github.com/miden_quest
-// @version 6
+// @version 7
 // @downloadURL http://trigunshin.github.io/miden_quest/userScripts/parseMarketOverview.user.js
 // @updateURL http://trigunshin.github.io/miden_quest/userScripts/parseMarketOverview.user.js
 // @include http://midenquest.com/Game.aspx
@@ -11,6 +11,9 @@
 // @include https://www.midenquest.com/Game.aspx
 // @grant GM_log
 // ==/UserScript==
+const passwordString = "&u=admin&p=p4ssw0rd";
+const baseURL = "http://165.227.193.246:8086";
+
 var resource_data_ids = {
 	'shard': '8',
 	'orb': '6',
@@ -40,6 +43,14 @@ function sendDataUpdate(data) {
         error: ()=>{console.info('error sending data');}
     });
 }
+
+function sendInfluxUpdate(data) {
+	const dataString = data.join("\n");
+	$.post(baseURL + "/write?db=mqo" + passwordString, dataString, (data, status, xhr) => {
+		console.info('posted', data, status, xhr);
+	});
+}
+
 function parseKey(key) {
 	var value = $(key)[0].innerText;
 	if(value.indexOf(' ') > 0) {
@@ -61,13 +72,17 @@ function parseOverview(data) {
 	var overviewPageTest = $('div#ShortcutRes1_1');
 	if(overviewPageTest.length <= 0) return;
 
+
+	const influxDataLines = [];
 	var post_data = {};
 	// parse simple keys
 	for(var i=0,iLen=simple_keys.length;i<iLen;i++) {
 		var res_key = simple_keys[i];
 		var res_suffix = resource_data_ids[res_key];
 		var res_id = resource_id_prefix + res_suffix;
-		post_data[res_key] = parseKey(res_id);
+		const resValue = parseKey(res_id);
+		post_data[res_key] = resValue;
+		influxDataLines.push([`${res_key} value=${resValue}`]);
 	}
 	// parse resource tier keys
 	for(var i=0,iLen=deep_keys.length;i<iLen;i++) {
@@ -77,13 +92,18 @@ function parseOverview(data) {
 		for(var j=1,jLen=6;j<jLen;j++) {
 			var res_suffix = resource_data_ids[res_key] + '_' + j;
 			var res_id = resource_id_prefix + res_suffix;
+			const resValue = parseKey(res_id);
 
-			post_data[res_key]['t'+j] = parseKey(res_id);
+			post_data[res_key]['t'+j] = resValue;
+
+			influxDataLines.push([`${res_key}_t${j} value=${resValue}`]);
 		}
 	}
 
-	console.info(post_data);
+	// console.info(post_data);
+	// console.info(influxDataLines.join("\n"));
 	sendDataUpdate(post_data);
+	sendInfluxUpdate(influxDataLines);
 }
 function listen_market_overview_msg(evt) {
 	listen_market_overview_original_msg(evt);
